54e582a2b67fa9b129ccf5791ab6889ec525f183
harkaled2 <80251702+ivan-molnar@users.noreply.github.com>
2022-03-21 13:33:13 +0100

    started ast

---
+++ src/ast/ast.go
@@ -0,0 +1,18 @@
+package ast
+
+// The base Node interface
+type Node interface {
+	TokenLiteral() string
+}
+
+// All statement nodes implement this
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// All expression nodes implement this
+type Expression interface {
+	Node
+	expressionNode()
+}
